d4d3ae0a19ca6d503f316f94d53a83b688c7f685
Ana Paula Moura <ana.mcruz@hotmail.com>
2023-11-21 16:56:54 -0300

    adicao de Conta Corrente e Poupanca

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -3,19 +3,39 @@ package conta;
 import java.util.Scanner;
 import conta.model.Conta;
 import conta.util.Cores;
+import conta.model.ContaPoupanca;
+import conta.model.ContaCorrente;
+
 
 
 
 public class Menu {
 	public static void main(String[] args) {
 		
-		//Teste da Classe CONTA
+		
 		Conta c1 = new Conta(1,123,1,"Adriana",10000.0f);
 		c1.visualizar();
 		c1.sacar(12000.0f);
 		c1.visualizar();
 		c1.depositar(5000.0f);
 		c1.visualizar();
+				
+		ContaCorrente 
+		cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+				
+		        
+		ContaPoupanca 
+		cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 		
 		Scanner leia = new Scanner(System.in);
 		int opcao;
