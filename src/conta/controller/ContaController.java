8edd56fabcc31f96ffa9f64146474c5fc15b0d88
Ana Paula Moura <ana.mcruz@hotmail.com>
2024-01-03 16:33:43 -0300

    security

--- src/conta/controller/ContaController.java
+++ src/conta/controller/ContaController.java
@@ -7,15 +7,11 @@ import conta.repository.ContaRepository;
 
 public class ContaController implements ContaRepository {
 
-    /**
-     *  Collection listaContas contendo Objetos do tipo Conta
-     * */
+    
     private ArrayList<Conta> listaContas = new ArrayList<Conta>();
     int numero = 0;
     
-    /**
-     *  Procurar Conta por numero
-     * */
+   
     @Override
     public void procurarPorNumero(int numero) {
         var conta = buscarNaCollection(numero);
@@ -26,28 +22,21 @@ public class ContaController implements ContaRepository {
 			System.out.println("\nA Conta número: " + numero + " não foi encontrada!");
     }
 
-    /**
-     *  Método Listar todas as Contas
-     * */
-    @Override
+        @Override
     public void listarTodas() {
         for (var conta : listaContas) {
 			conta.visualizar();
 		}        
     }
 
-    /** 
-     * Método Cadastrar no Conta
-     * */
+   
     @Override
     public void cadastrar(Conta conta) {
 		listaContas.add(conta);
 		System.out.println("\nA Conta número: " + conta.getNumero() + " foi criada com sucesso!");
     }
 
-    /**
-     * Atualizar dados da Conta
-     * */
+    
     @Override
     public void atualizar(Conta conta) {
         var buscaConta = buscarNaCollection(conta.getNumero());
@@ -59,9 +48,7 @@ public class ContaController implements ContaRepository {
 			System.out.println("\nA Conta numero: " + conta.getNumero() + " não foi encontrada!");
     }
 
-    /**
-     *  Apagar Conta
-     * */
+   
     @Override
     public void deletar(int numero) {
         var conta = buscarNaCollection(numero);
@@ -91,20 +78,12 @@ public class ContaController implements ContaRepository {
         
     }
 
-    /** 
-	 * Métodos Auxiliares
-	 **/
-	
-	/**
-	 * Método para gerar automaticamente o Número da Conta
-	 * */
+    
 	public int gerarNumero() {
 		return ++ numero;
 	}
 
-    /**
-	 * Método para buscar a Conta na Collection
-	 * */
+    
 	public Conta buscarNaCollection(int numero) {
 		for (var conta : listaContas) {
 			if (conta.getNumero() == numero) {
@@ -115,9 +94,7 @@ public class ContaController implements ContaRepository {
 		return null;
 	}
 
-	/**
-	 * Método para retornar o Tipo da Conta
-	 * */
+	
 	public int retornaTipo(int numero) {
 		for (var conta : listaContas) {
 			if (conta.getNumero() == numero) {
